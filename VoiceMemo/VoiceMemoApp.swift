a5b2445dd0cab3fb1ec5cdaa2df5608456658059
torinunna <torinunna9588@gmail.com>
2024-03-29 22:58:25 +0900

    Initial Commit

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by YUJIN KWON on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
